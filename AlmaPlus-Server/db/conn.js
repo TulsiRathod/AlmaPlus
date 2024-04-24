c10765ae77da8ba03744628704721c41bf6dba56
Tulsi Rathod <105197561+TulsiRathod@users.noreply.github.com>
2024-04-24 17:01:35 +0530

    Update conn.js

--- AlmaPlus-Server/db/conn.js
+++ AlmaPlus-Server/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToMongo = async () => {
     try {
         // mongodb+srv://drashtidankhara7:<password>@cluster0.jcwxk8q.mongodb.net/test
-        await mongoose.connect("mongodb+srv://drashtidankhara7:drashtidankhara7@cluster0.jcwxk8q.mongodb.net/AlmaPlus?retryWrites=true&w=majority");
+        await mongoose.connect("mongodb+srv://rudra:Rudra@cluster0.wshjk1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Connected To AlmaPlus !!!");
     } catch (error) {
         console.log("Error in connection : ", error);

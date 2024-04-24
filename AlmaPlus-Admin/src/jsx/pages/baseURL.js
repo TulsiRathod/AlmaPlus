7533442c1982b93fbd500f52fb7b6e924852808c
rudra2601 <rudrabhatt9531@gmail.com>
2024-04-24 11:43:26 +0530

    ,

---
+++ AlmaPlus-Admin/src/jsx/pages/baseURL.js
@@ -0,0 +1,4 @@
+// export const ALMA_PLUS_API_URL = "http://alma-plus-server.vercel.app/";
+export const ALMA_PLUS_API_URL = "http://localhost:5000";
+
+// export const ALMA_PLUS_API_URL = "http://20.106.152.112:5000/";
